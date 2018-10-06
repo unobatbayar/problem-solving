cf63437b34d4a07369b200f9ba79d695121333af
unobatbayar <unobatbayar@yahoo.com>
2018-10-06 23:34:52 +0100

    front_back solvedgit add --all

---
+++ Python/Warmup-1/front_back.py
@@ -0,0 +1,26 @@
+"""
+Given a string, return a new string where the first and last chars have been exchanged.
+
+
+front_back('code') → 'eodc'
+front_back('a') → 'a'
+front_back('ab') → 'ba'
+
+@author unobatbayar
+
+"""
+
+def front_back(str):
+  if len(str) == 1:
+    return str
+  elif len(str) > 1:
+    array = list(str)
+    array[0], array[len(str)-1] = array[len(str)-1], array[0]
+    newstring = ""
+    for x in array:
+      newstring = newstring + x
+    
+    str = newstring
+    return str
+  else:
+    return str
