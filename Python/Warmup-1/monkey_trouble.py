1cff45b25d3b06a8045e2d9a7dea1d5e77f2eb33
unobatbayar <unobatbayar@yahoo.com>
2018-10-06 23:27:50 +0100

    new solution

---
+++ Python/Warmup-1/monkey_trouble.py
@@ -0,0 +1,18 @@
+""" 
+We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+
+
+monkey_trouble(True, True) → True
+monkey_trouble(False, False) → True
+monkey_trouble(True, False) → False
+
+@author unobatbayar
+"""
+
+def monkey_trouble(a_smile, b_smile):
+  if a_smile == True and b_smile == True:
+    return True
+  elif a_smile == False and b_smile == False:
+    return True
+  else:
+    return False
