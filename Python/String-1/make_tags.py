76da9f912f42f39663860f323db026c297b2d99b
unobatbayar <unobatbayar@yahoo.com>
2018-10-08 22:52:10 +0100

    new program

---
+++ Python/String-1/make_tags.py
@@ -0,0 +1,17 @@
+"""
+
+The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+
+
+make_tags('i', 'Yay') → '<i>Yay</i>'
+make_tags('i', 'Hello') → '<i>Hello</i>'
+make_tags('cite', 'Yay') → '<cite>Yay</cite>'
+
+@author unobatbayar
+"""
+
+def make_tags(tag, word):
+  html_first = "<" + tag + ">"
+  html_last = "</" + tag + ">"
+  return html_first + word + html_last
+  
