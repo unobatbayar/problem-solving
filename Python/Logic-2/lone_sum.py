374e943d6122a8fa8dfd44ca074274bafc4790db
unobatbayar <unobatbayar@yahoo.com>
2018-10-10 22:54:43 +0100

    new program

---
+++ Python/Logic-2/lone_sum.py
@@ -0,0 +1,20 @@
+"""
+
+
+Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum.
+
+
+lone_sum(1, 2, 3) → 6
+lone_sum(3, 2, 3) → 2
+lone_sum(3, 3, 3) → 0
+
+@author unobatbayar
+
+"""
+def lone_sum(a, b, c):
+  sum = 0
+  if a != b and a != c: sum += a
+  if b != a and b != c: sum += b
+  if c != a and c != b: sum += c
+
+  return sum
---
+++ Python/Logic-2/lucky_Sum.py
@@ -0,0 +1,20 @@
+"""
+
+Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
+
+
+lucky_sum(1, 2, 3) → 6
+lucky_sum(1, 2, 13) → 3
+lucky_sum(1, 13, 3) → 1
+
+@author unobatbayar
+
+"""
+def lucky_sum(a, b, c):
+  if a == 13:
+    return 0
+  elif b == 13:
+    return a
+  elif c == 13:
+    return a + b
+  return a + b + c
