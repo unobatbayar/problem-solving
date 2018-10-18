3b8d1ef6c805bf3e029110916f299e2109218dad
unobatbayar <unobatbayar@yahoo.com>
2018-10-18 20:50:20 +0100

    warmup-1 finished

---
+++ Python/Warmup-1/missing_char.py
@@ -0,0 +1,16 @@
+"""
+
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+
+
+missing_char('kitten', 1) → 'ktten'
+missing_char('kitten', 0) → 'itten'
+missing_char('kitten', 4) → 'kittn'
+
+@author unobatbayar
+
+"""
+def missing_char(str, n):
+  first = str[:n]
+  last = str[n+1:]
+  return first+last
