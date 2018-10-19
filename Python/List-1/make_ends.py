39e5e77fe00ca0aea31694844a5b5aa6d63a99d6
unobatbayar <unobatbayar@yahoo.com>
2018-10-19 22:07:47 +0100

    done

---
+++ Python/List-1/make_ends.py
@@ -0,0 +1,17 @@
+"""
+
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+
+
+make_ends([1, 2, 3]) → [1, 3]
+make_ends([1, 2, 3, 4]) → [1, 4]
+make_ends([7, 4, 6, 2]) → [7, 2]
+
+"""
+
+def make_ends(nums):
+  if len(nums) > 1:
+    new = [nums[0], nums[len(nums)-1]]
+    return new
+  new = [nums[0], nums[0]]
+  return new
