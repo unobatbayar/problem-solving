16f40792ba3496d6f5b23b90e72d257213afb19a
unobatbayar <unobatbayar@yahoo.com>
2018-10-19 21:55:22 +0100

    string-1 7th prob done

---
+++ Python/String-1/hello_name.py
@@ -0,0 +1,15 @@
+"""
+
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+hello_name('Bob') → 'Hello Bob!'
+hello_name('Alice') → 'Hello Alice!'
+hello_name('X') → 'Hello X!'
+
+@author unobatbayar
+
+"""
+
+def hello_name(name):
+  return 'Hello ' + name + '!'
