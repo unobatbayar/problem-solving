7f2b407898dc2355c3067c012f5d94942d97bc78
unobatbayar <unobatbayar@yahoo.com>
2018-10-19 21:58:24 +0100

    done 1 more

---
+++ Python/String-1/non_start.py
@@ -0,0 +1,17 @@
+"""
+
+Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+
+non_start('Hello', 'There') → 'ellohere'
+non_start('java', 'code') → 'avaode'
+non_start('shotl', 'java') → 'hotlava'
+
+@author unobatbayar
+
+"""
+
+def non_start(a, b):
+  first = a[1:]
+  last = b[1:]
+  return first + last
