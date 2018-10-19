816a17dbaac5abe9b7d32258d320e7b97642a179
unobatbayar <unobatbayar@yahoo.com>
2018-10-19 22:04:19 +0100

    done reverse3

---
+++ Python/List-1/reverse3.py
@@ -0,0 +1,15 @@
+"""
+Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+
+
+reverse3([1, 2, 3]) → [3, 2, 1]
+reverse3([5, 11, 9]) → [9, 11, 5]
+reverse3([7, 0, 0]) → [0, 0, 7]
+
+@author unobatbayar
+
+"""
+
+def reverse3(nums):
+  reversed = [nums[2], nums[1], nums[0]]
+  return reversed
