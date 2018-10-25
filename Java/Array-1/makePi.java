6936ce196c55875dde0bfaa2bce35cea14fb8d5c
unobatbayar <uno02bt@gmail.com>
2018-10-25 21:57:53 +0100

    same probl with python done

---
+++ Java/Array-1/makePi.java
@@ -0,0 +1,11 @@
+/*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+makePi() → [3, 1, 4]
+*/
+
+public int[] makePi() {
+ int[] pi = new int[]{ 3,1,4 }; 
+ return pi;
+}
+
