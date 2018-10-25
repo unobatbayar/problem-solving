42ae4b6a0affc48c496034be42a0992ec2110e56
unobatbayar <uno02bt@gmail.com>
2018-10-25 21:58:42 +0100

    done

---
+++ Java/Array-1/sum3.java
@@ -0,0 +1,13 @@
+/*
+Given an array of ints length 3, return the sum of all the elements.
+
+sum3([1, 2, 3]) → 6
+sum3([5, 11, 2]) → 18
+sum3([7, 0, 0]) → 7
+
+*/
+
+public int sum3(int[] nums) {
+  return nums[0] + nums[1] + nums[2];
+}
+
