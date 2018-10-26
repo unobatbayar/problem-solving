2f3d41481feda5603bd3b2542fe1ecfac62a7922
unobatbayar <unobatbayar@yahoo.com>
2018-10-26 22:35:21 +0100

    added 3rd problem

---
+++ Java/String-1/helloName.java
@@ -0,0 +1,16 @@
+/*
+
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"
+*/
+
+class helloName{
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+      }
+      
+}
