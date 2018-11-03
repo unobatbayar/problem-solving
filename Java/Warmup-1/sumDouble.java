6c0c561e6c18109140e79359a509961dc5c817ba
unobatbayar <unobatbayar@yahoo.com>
2018-11-03 21:13:15 +0000

    finished

---
+++ Java/Warmup-1/sumDouble.java
@@ -0,0 +1,16 @@
+/*
+
+Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8
+*/
+
+class sumDouble{
+    public int sumDouble(int a, int b) {
+        if(a == b) return (a+b)*2;
+        return(a+b);
+      }      
+}
