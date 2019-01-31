1d953f87d67c9a066c4f8b96d9b848bc03ca2dc1
unobatbayar <uno02bt@gmail.com>
2019-01-31 22:26:43 +0000

    done

---
+++ Java/Warmup-2/stringYak.java
@@ -0,0 +1,14 @@
+/**
+ Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, but the "a" can be any char. The "yak" strings will not overlap.
+
+
+stringYak("yakpak") → "pak"
+stringYak("pakyak") → "pak"
+stringYak("yak123ya") → "123ya"
+ */
+
+public String stringYak(String str) {
+    String result = str.replace("yak", "");
+    return result;
+  }
+  
