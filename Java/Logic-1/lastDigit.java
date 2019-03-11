c643017caa94926f97afcc6a09ce78894fd339ce
unobatbayar <uno02bt@gmail.com>
2019-03-11 21:26:45 +0000

    done lastDigit, beautiful code

---
+++ Java/Logic-1/lastDigit.java
@@ -0,0 +1,19 @@
+/**
+ * Given three ints, a b c, return true if two or more of them have the same rightmost digit. The ints are non-negative. Note: the % "mod" operator computes the remainder, e.g. 17 % 10 is 7.
+
+lastDigit(23, 19, 13) → true
+lastDigit(23, 19, 12) → false
+lastDigit(23, 19, 3) → true
+@author unobatbayar
+ */
+
+public boolean lastDigit(int a, int b, int c) {
+    boolean ab = a%10 == b%10;
+    boolean bc = b%10 == c%10;
+    boolean ac = a%10 == c%10;
+    
+    if(ab || bc || ac) return true;
+    return false;
+    
+  }
+  
