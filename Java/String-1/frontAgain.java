eeb76af71683c7eb623dddce7283633157a6a2b7
unobatbayar <uno02bt@gmail.com>
2019-03-13 17:29:43 +0000

    done frontAgain

---
+++ Java/String-1/frontAgain.java
@@ -0,0 +1,13 @@
+/**
+ * Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited".
+
+frontAgain("edited") → true
+frontAgain("edit") → false
+frontAgain("ed") → true
+@author unobatbayar
+ */
+public boolean frontAgain(String str) {
+    if(str.length()>= 2 && str.substring(0, 2).equals(str.substring(str.length()-2, str.length()))) return true;
+    return false;
+  }
+  
