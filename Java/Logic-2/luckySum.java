25233c7e67cb06c74143acc0dc597e1840050a8f
unobatbayar <uno02bt@gmail.com>
2019-03-14 04:50:26 +0000

    fixed description on luckySum

--- Java/Logic-2/luckySum.java
+++ Java/Logic-2/luckySum.java
@@ -1,15 +1,9 @@
 /**
- public int luckySum(int a, int b, int c) {
-  if a == 13{
-    return 0;
-  }
-  else if b == 13{
-    return a;
-  }
-  else{
-    return a + b;
-  }
-}
+Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
+
+luckySum(1, 2, 3) → 6
+luckySum(1, 2, 13) → 3
+luckySum(1, 13, 3) → 1
 @author unobatbayar
  */
 
