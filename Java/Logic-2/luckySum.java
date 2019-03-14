fd997d947ef2ddf40b56da97945c960f24194e1e
unobatbayar <uno02bt@gmail.com>
2019-03-14 04:51:47 +0000

    code improvement on luckySum

--- Java/Logic-2/luckySum.java
+++ Java/Logic-2/luckySum.java
@@ -7,18 +7,10 @@ luckySum(1, 13, 3) → 1
 @author unobatbayar
  */
 
-class luckySum{
-    public int luckySum(int a, int b, int c) {
-        if (a == 13) {
-          return 0;
-        }
-        else if (b == 13) {
-          return a;
-        }
-        else if (c == 13){
-          return a + b;
-        }
-        return a + b + c;
-      }
-      
+public int luckySum(int a, int b, int c) {
+  if (a == 13) return 0;
+  else if (b == 13) return a;
+  else if (c == 13) return a + b;
+  
+  return a + b + c;
 }
