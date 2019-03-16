aa2d05398c05b44125eda50699153f4e4243d38e
unobatbayar <uno02bt@gmail.com>
2019-03-16 17:32:17 +0000

    done plusTwo

---
+++ Java/Array-1/plusTwo.java
@@ -0,0 +1,12 @@
+/**
+ * Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+
+plusTwo([1, 2], [3, 4]) → [1, 2, 3, 4]
+plusTwo([4, 4], [2, 2]) → [4, 4, 2, 2]
+plusTwo([9, 2], [3, 4]) → [9, 2, 3, 4]
+
+ */
+public int[] plusTwo(int[] a, int[] b) {
+    return new int[]{a[0], a[1], b[0], b[1]};
+  }
+  
