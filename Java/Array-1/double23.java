40edec028d0a5d91f68b166d28ebf0f97b02cc2c
unobatbayar <uno02bt@gmail.com>
2019-03-16 17:34:45 +0000

    done double23

---
+++ Java/Array-1/double23.java
@@ -0,0 +1,14 @@
+/**
+ * Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.
+
+double23([2, 2]) → true
+double23([3, 3]) → true
+double23([2, 3]) → false
+
+ */
+public boolean double23(int[] nums) {
+    if((nums.length >= 2 && nums[0] == nums[1]) && (nums[0] == 2 || nums[1] == 3)) return true;
+    return false;
+  }
+
+  
