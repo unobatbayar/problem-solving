a776fabf22bf88c31112b64f40caff678d8cd5f5
unobatbayar <uno02bt@gmail.com>
2019-03-16 17:29:34 +0000

    done unlucky1

---
+++ Java/Array-1/unlucky1.java
@@ -0,0 +1,14 @@
+/**
+ We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1. Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
+
+unlucky1([1, 3, 4, 5]) → true
+unlucky1([2, 1, 3, 4, 5]) → true
+unlucky1([1, 1, 1]) → false
+ */
+public boolean unlucky1(int[] nums) {
+    if (nums.length < 2) return false;
+    if((nums[0] == 1 && nums[1] == 3) || nums[1] == 1 && nums[2] == 3) return true;
+    if(nums[nums.length-2] == 1 && nums[nums.length-1] == 3) return true;
+    return false;
+  }
+  
