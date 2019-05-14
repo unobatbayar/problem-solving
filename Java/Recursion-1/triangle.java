ea284640cb9ca142a1d465807c125a4140c81886
unobatbayar <uno02bt@gmail.com>
2019-05-14 09:02:21 +0100

    done

---
+++ Java/Recursion-1/triangle.java
@@ -0,0 +1,13 @@
+/*
+We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
+
+triangle(0) → 0
+triangle(1) → 1
+triangle(2) → 3
+@author unobatbayar
+*/
+public int triangle(int rows) {
+    if(rows == 0) return 0;
+    return rows + triangle(rows-1);
+  }
+  
