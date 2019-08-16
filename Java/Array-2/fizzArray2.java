c12b5326266fc4b4f04a5699c92c9bfc4e1acf1d
unobatbayar <uno02bt@gmail.com>
2019-08-16 02:04:33 +0100

    feat: solve new problem and add solution

---
+++ Java/Array-2/fizzArray2.java
@@ -0,0 +1,20 @@
+/**
+ * Given a number n, create and return a new string array of length n, containing the strings "0", "1" "2" .. through n-1. N may be 0, in which case just return a length 0 array. Note: String.valueOf(xxx) will make the String form of most types. The syntax to make a new string array is: new String[desired_length]  (See also: FizzBuzz Code)
+
+fizzArray2(4) → ["0", "1", "2", "3"]
+fizzArray2(10) → ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+fizzArray2(2) → ["0", "1"]
+
+@author unobatbayar
+ */
+
+public String[] fizzArray2(int n) {
+    String[] array = new String[n];
+    
+    for(int i = 0; i<n; i++){
+      array[i] = String.valueOf(i);
+    }
+    
+    return array;
+  }
+  
