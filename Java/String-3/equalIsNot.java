172b435f4e4aeeb77b611e4ae89eec47eb5ec25c
unobatbayar <uno@konple.com>
2019-12-15 00:57:11 +0800

    style: comment update

--- Java/String-3/equalIsNot.java
+++ Java/String-3/equalIsNot.java
@@ -17,24 +17,26 @@ public boolean equalIsNot(String str) {
   }
   
 
-//TEST CODE 
+/*
+TEST CODE 
 
-// class equalIsNot{
-//     public static void main (String[] args){
-//         System.out.println(test("isnotno7Not"));
-//     }
+class equalIsNot{
+    public static void main (String[] args){
+        System.out.println(test("isnotno7Not"));
+    }
 
-//     public static boolean test(String str) {
+    public static boolean test(String str) {
   
-//         int without_is = (str.length() - str.replace("is", "").length())/2;
+        int without_is = (str.length() - str.replace("is", "").length())/2;
 
-//         System.out.println("NOT LENGTH: " + str.length() + "TEST STRING: " + str);
-//         System.out.println("IS REMOVED: " + str.replace("is", "").length() + " IS LENGTH: " + (str.length() - str.replace("is", "").length()) + " IS COUNT: " + without_is);
-//         int without_not = (str.length() - str.replace("not", "").length())/3;
-//         System.out.println("NOT REMOVED: " + str.replace("not", "").length() + " NOT LENGTH: " + (str.length() - str.replace("not", "").length()) + " NOT COUNT: " + without_not);
+        System.out.println("NOT LENGTH: " + str.length() + "TEST STRING: " + str);
+        System.out.println("IS REMOVED: " + str.replace("is", "").length() + " IS LENGTH: " + (str.length() - str.replace("is", "").length()) + " IS COUNT: " + without_is);
+        int without_not = (str.length() - str.replace("not", "").length())/3;
+        System.out.println("NOT REMOVED: " + str.replace("not", "").length() + " NOT LENGTH: " + (str.length() - str.replace("not", "").length()) + " NOT COUNT: " + without_not);
 
       
-//         return (without_is == without_not);  
-//       }
+        return (without_is == without_not);  
+      }
       
-// }
+}
+*/
