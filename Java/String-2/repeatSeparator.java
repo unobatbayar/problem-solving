38832d0a4d363e43058238d48e70b0126d600873
Uno Batbayar <38866249+unobatbayar@users.noreply.github.com>
2020-05-25 22:18:35 +0800

    chore: repatSeparator

--- Java/String-2/repeatSeparator.java
+++ Java/String-2/repeatSeparator.java
@@ -1,11 +1,11 @@
 public String repeatSeparator(String word, String separator, int count) {
   //solution by Aniket Sahrawat
-  //return String.join(separator, Collections.nCopies(count, word));
+  return String.join(separator, Collections.nCopies(count, word));
   
   //my solution
-  if(count == 0) return "";
-  String result = word;
-  for(int i = 0; i<count-1; i++) result += separator + word;
+  // if(count == 0) return "";
+  // String result = word;
+  // for(int i = 0; i<count-1; i++) result += separator + word;
   
-  return result;
+  // return result;
 }
