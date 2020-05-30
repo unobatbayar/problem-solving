e491671225ec43197faf9e6ac6894b029ddfb59c
Uno Batbayar <38866249+unobatbayar@users.noreply.github.com>
2020-05-30 18:59:07 +0800

    feat: solve mixString

---
+++ Java/String-2/mixString.java
@@ -0,0 +1,40 @@
+/**
+
+Given two strings, a and b, create a bigger string made of the first char of a, the first char of b, the second char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
+
+
+mixString("abc", "xyz") → "axbycz"
+mixString("Hi", "There") → "HTihere"
+mixString("xxxx", "There") → "xTxhxexre"
+
+@author unobatbayar
+**/
+
+public String mixString(String a, String b) {
+  int length = a.length() + b.length();
+  boolean a_turn = true;
+  String result = "";
+  
+  for(int i = 0; i<length; i++){
+    if(a_turn){
+      if(a.equals("")){
+        result += b;
+        break;
+      }
+      result += a.substring(0, 1);
+      a = a.substring(1);
+      a_turn = false;
+    }
+    else{
+      if(b.equals("")){
+        result += a;
+        break;
+      }
+      result += b.substring(0, 1);
+      b = b.substring(1);
+      a_turn = true;
+    }
+  }
+  
+  return result;
+}
