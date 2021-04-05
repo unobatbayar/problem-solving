6b161ed5de99161a926ee4b7e45422fd547d495c
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-04-05 13:06:55 +0900

    fix: rename variable

--- HackerRank/C++/Conditionals.cpp
+++ HackerRank/C++/Conditionals.cpp
@@ -9,9 +9,9 @@ int main()
     int n;
     cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    string number_words[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    string numerics[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     
-    if(n <= 9) cout << number_words[n-1];
+    if(n <= 9) cout << numerics[n-1];
     else cout << "Greater than 9";
     
     // Write Your Code Here
