bab1cb12577e43f686849a10d20a43f5bb52901e
unobatbayarls <uno02bt@gmail.com>
2021-04-15 00:49:52 +0900

    feat: solve my first project euler problem

---
+++ Projecteuler/p1.cpp
@@ -0,0 +1,24 @@
+/**
+
+
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+**/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int sum = 0;
+    for(int i = 1; i<1000; i++){
+        if(i%3 == 0 || i%5 == 0) sum += i;
+    }
+    cout << "Sum of all the multiples of 3 or 5 below 1000: " << sum;
+    return 0;
+}
