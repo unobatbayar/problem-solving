359f07ad9d89c2b9a02d5d949ffbb21923452ddc
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2021-04-20 11:12:43 +0900

    feat: do functions in c

---
+++ HackerRank/C/Functions.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+/*
+
+At https://www.hackerrank.com/challenges/functions-in-c/problem
+
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d){
+    int numbers[] = {a, b, c, d};
+    int max = 0;
+    
+    for(int i = 0; i<sizeof(numbers)/sizeof(int); i++){
+        if(numbers[i] > max) max = numbers[i];
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
