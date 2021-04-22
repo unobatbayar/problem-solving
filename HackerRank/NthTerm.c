eb0f27a784e69b10ed7e8e78aa63d00f5f266373
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-04-22 17:04:29 +0900

    feat: solve calculate nth term

---
+++ HackerRank/NthTerm.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+    if(n - 1 == 3) return a + b + c;
+    return find_nth_term(n-1, a+b+c, b, c);
+  //Write your code here.
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
