115f99ad99f56f07628b6b48fed8f8a21ec0286a
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-04-23 11:10:33 +0900

    feat: solve sum of digits of a five

---
+++ HackerRank/C/SumOfDigitsOfAFive.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    while(n != 0){
+        sum += n%10;
+        n = n/10;
+    }
+    printf("%d", sum);
+    return 0;
+}
