b1bec6a54ddd143dc27caadbe669ac394aa84460
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-04-26 10:18:34 +0900

    feat: remove comments

--- HackerRank/C/BoxesThroughTunnel.c
+++ HackerRank/C/BoxesThroughTunnel.c
@@ -4,9 +4,6 @@
 
 struct box
 {
-	/**
-	* Define three fields of type int: length, width and height
-	*/
     int length;
     int width;
     int height;
@@ -16,15 +13,9 @@ typedef struct box box;
 
 int get_volume(box b) {
     return b.length * b.width * b.height;
-	/**
-	* Return the volume of the box
-	*/
 }
 
 int is_lower_than_max_height(box b) {
-	/**
-	* Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
-	*/
     if(b.height < MAX_HEIGHT) return 1;
     return 0;
 }
