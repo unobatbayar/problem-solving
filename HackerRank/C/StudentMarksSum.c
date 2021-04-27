bec5c4e6eb79c53c6c7d910237619ad2ba19a19a
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-04-27 09:29:06 +0900

    feat: further improve code

--- HackerRank/C/StudentMarksSum.c
+++ HackerRank/C/StudentMarksSum.c
@@ -6,13 +6,12 @@
 //Complete the following function.
 
 int marks_summation(int* marks, int number_of_students, char gender) {
-  //Write your code here.
-  int sum = 0;
-  for(int i = 0; i<number_of_students; i++){
-      if(gender == 'b' && i%2 == 0) sum += marks[i];
-      else if(gender == 'g' && i%2 != 0) sum += marks[i];
-  }
-  return sum;
+    //Write your code here.
+    int sum = 0;
+    for(int i = gender == 'g' ? 1: 0; i<number_of_students; i+=2){
+        sum += marks[i];
+    }
+    return sum;
 }
 
 int main() {
