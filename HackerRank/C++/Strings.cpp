ebfc3d9e9475edb7e899117617b854313806a396
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-04-28 11:46:42 +0900

    feat: solve strings cpp

---
+++ HackerRank/C++/Strings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a, b;
+    cin >> a >> b;
+    
+    cout << a.size() << " " << b.size() << '\n';
+    cout << a << b << '\n';
+    
+    //swap first char
+    char first = a[0];
+    a[0] = b[0];
+    b[0] = first;
+    
+    cout << a << " " << b;
+    return 0;
+}
