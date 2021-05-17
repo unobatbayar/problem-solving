322009a10dc88cbd81e2ddf03a87de4d7970b6dd
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-05-18 07:06:08 +0900

    feat: solve basic data types

---
+++ HackerRank/C/BasicDataTypes.c
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    //int, long, char, float, and double
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    
+    scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+    printf("%d \n%ld \n%c \n%f \n%lf", i, l, c, f, d);
+    
+    return 0;
+}
