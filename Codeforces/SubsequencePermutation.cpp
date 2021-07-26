2c705227f48e25d91de8764d99a907713a4dd534
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-07-26 13:55:46 +0900

    feat: solve subsequence permutation

---
+++ Codeforces/SubsequencePermutation.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int tests;
+    cin >> tests;
+
+    while(tests--){
+        int length;
+        string s;
+
+        cin >> length >> s;
+
+        string sorted = s;
+        sort(sorted.begin(), sorted.end());
+
+        int k = 0;
+        for(int i = 0; i < length; i++){
+            if(s[i] != sorted[i]) k++;
+        }
+
+        cout << k << endl;
+    
+    }
+
+
+    return 0;
+}
