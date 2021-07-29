41e93b7500f1d80b179fbc5c1c4be7b2cbdf0aaa
unobatbayar <526473+gb96@users.noreply.github.com>
2021-07-29 15:16:16 +0900

    chore: well..

---
+++ Codeforces/MaximumCostDeletion.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>     // std::cout, std::cin
+#include <algorithm>    // std::unique, std::distance, std::max
+using namespace std;
+
+int main()
+{
+    int tests;
+    cin >> tests;
+
+    while(tests--){
+        int n, a, b; 
+        string s;
+        cin >> n >> a >> b >> s;
+        int m = unique(s.begin(), s.end()) - s.begin();
+
+        cout << "What?: " <<n * a << "Huh?: " << max(n * b, (m / 2 + 1) * b) << endl;
+
+    }
+
+
+    return 0;
+}
