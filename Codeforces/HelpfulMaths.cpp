817e0e672aa64871013bcd6475472036ced35553
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-08-17 11:07:40 +0900

    feat: solve problem

---
+++ Codeforces/HelpfulMaths.cpp
@@ -0,0 +1,29 @@
+#include <iostream>  // std::cout, std::cin
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string sum;
+    cin >> sum;
+
+    int length = (sum.length() + 1)/2;
+    int numbers[length];
+
+    int count = 0;
+    for(int i = 0; i<sum.length(); i++){
+        if(sum[i] != '+'){
+            numbers[count] = sum[i] - '0';
+            count++;
+        }
+    }
+
+    sort(numbers, numbers + length);
+
+    for(int i = 0; i<length; i++){
+        cout << numbers[i];
+        if(i+1<length) cout << '+';
+    }
+
+    return 0;
+}
