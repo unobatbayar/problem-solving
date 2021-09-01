350c1698e3802104160411c09547939c1c2f26cf
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2021-09-01 09:42:54 +0900

    feat: solve brother and bear

---
+++ Codeforces/BrotherAndBear.cpp
@@ -0,0 +1,19 @@
+﻿/** 
+ * @author unobatbayar 
+ */
+#include <iostream>
+using namespace std;
+
+int solve(int limak, int bob){
+    if(limak > bob) return 1;
+    return 2 + solve(limak*3, bob*2);
+}
+
+int main()
+{
+    int limak, bob;
+    cin >> limak >> bob;
+
+    cout << solve(limak, bob);
+    return 0;
+}
