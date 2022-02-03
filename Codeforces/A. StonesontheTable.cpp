597f34604be5d15c030c4fc010258cb7a8c8442f
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2022-02-03 16:51:19 +0900

    feat: solve stones on the table

---
+++ Codeforces/A. StonesontheTable.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int length;
+	cin >> length;
+	
+	int total = 0;
+	
+	// to return
+	char current = ' ';
+	
+    while(length--){
+        
+        // get input
+        char input;
+        cin >> input;
+        
+        // main logic
+        if(current == input) total++;
+        else {
+            current = input;
+        }
+        
+    }
+    
+    cout << total << endl;
+	return 0;
+}
