2afb5d46fe385b92e139e7943ddf20eadf2cc3cc
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2022-02-08 18:28:38 +0900

    feat: solve anton and danik

--- Codeforces/AntonAndDanik.cpp
+++ Codeforces/AntonAndDanik.cpp
@@ -1,31 +1,31 @@
-    #include <iostream>
-    using namespace std;
-    
-    string find_winner(int w[]){
-        if(w[0] > w[1]){
-            return "Anton";
-        }
-        else if(w[1] > w[0]){
-            return "Danik";
-        }
-        return "Friendship";
+#include <iostream>
+using namespace std;
+
+string find_winner(int w[]){
+    if(w[0] > w[1]){
+        return "Anton";
+    }
+    else if(w[1] > w[0]){
+        return "Danik";
+    }
+    return "Friendship";
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    string games;
+    cin >> games;
+
+    int w[2] = {0, 0};
+
+    for(int i = 0; i<n; i++){
+        if(games[i] == 'A') w[0]++;
+        else w[1]++;
     }
 
-    int main() {
-        int n;
-        cin >> n;
+    cout << find_winner(w) << endl;
 
-        string games;
-        cin >> games;
-
-        int w[2] = {0, 0};
-
-        for(int i = 0; i<n; i++){
-            if(games[i] == 'A') w[0]++;
-            else w[1]++;
-        }
-
-        cout << find_winner(w) << endl;
-        
-    	return 0;
-    }
+    return 0;
+}
