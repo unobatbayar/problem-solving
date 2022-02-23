33f7c20a344366f98ea630463ad4b0d2da2c9cef
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2022-02-24 07:14:21 +0900

    refactor: remove comments

--- CodingBat/Python/Logic-1/cigar_party.py
+++ CodingBat/Python/Logic-1/cigar_party.py
@@ -14,19 +14,3 @@ def cigar_party(cigars, is_weekend):
     return (cigars >= 40)
   else:
     return (cigars >= 40 and cigars <= 60)
-
-  
-""" OLD CODE
-def cigar_party(cigars, is_weekend):
-  if is_weekend == True:
-    if cigars >= 40:
-      return True
-    else:
-      return False
-  else:
-    if cigars >= 40 and cigars <= 60:
-      return True
-    else:
-      return False
-
-"""
