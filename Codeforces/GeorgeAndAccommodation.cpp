73817456d553a5b82fe42f5ce1ccc722030703b1
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2022-02-24 18:00:40 +0900

    feat: solve george and accommodation

---
+++ Codeforces/GeorgeAndAccommodation.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 0, p = 0, q = 0, a = 0;
+    cin >> n;
+
+    while(n--){
+        cin >> p >> q;
+        if(q - p >= 2) a++;
+    }
+
+    cout << a;
+    
+    return 0;
+}
