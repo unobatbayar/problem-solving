c3497a5738e04dd84b31fe9ce9e09d8936070ca5
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2022-03-01 13:43:07 +0900

    feat: solve Presents

---
+++ Codeforces/Presents.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    int n = 0;
+    cin >> n;
+
+    // using unordered map for O(1) complexity
+    unordered_map<int, int> exchanges; 
+
+    for(int i = 1; i<=n; ++i){
+        int p = 0;
+        cin >> p;
+        exchanges.insert(make_pair(p, i));
+    }
+
+    for(int i = 1; i<=n; ++i){
+        cout << exchanges[i] << endl;
+    }
+    
+    return 0;
+}
