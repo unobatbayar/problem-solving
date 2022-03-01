e7d7ce3f45803f0d21a913b68c740858e1241231
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2022-03-01 16:50:59 +0900

    feat: solve Magnets, todo: improve 154 ms time

---
+++ Codeforces/Magnets.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 0, counter = 1;
+    cin >> n;
+
+    char last = ' ';
+    
+    while(n--){
+        string pole;
+        cin >> pole;
+        if(last == ' '){
+            last = pole[0];
+        }
+        else{
+            if(last != pole[0]) counter++;
+            last = pole[0];
+        }
+    }    
+    cout << counter << endl;
+    return 0;
+}
