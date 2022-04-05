59d33e1fa1fe31c9b6c5d627e4fc0360f8bf3ea5
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2022-04-05 16:41:43 +0900

    feat: solve reverse linkedlist (iterative + recursive)
    iterative seems 4 ms faster

--- LeetCode/ReverseLinkedList.cpp
+++ LeetCode/ReverseLinkedList.cpp
@@ -1,4 +1,5 @@
 /**
+ * Iterative approach
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -28,5 +29,36 @@ public:
         }
         
         return previous;
+    }
+};
+
+/**
+ * Recursive approach
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        return reverse(nullptr, head);
+    }
+    
+private:
+    ListNode* reverse(ListNode* previous, ListNode* head){
+        if(head == nullptr){
+            return previous;
+        }
+        
+        ListNode* next = head->next;
+        
+        head->next = previous;
+                    
+        return reverse(head, next);
     }
 };
