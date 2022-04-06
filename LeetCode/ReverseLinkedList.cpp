05fe7d569353cb60e7c8297cdc325102ffd95482
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2022-04-06 14:22:25 +0900

    chore: update

--- LeetCode/ReverseLinkedList.cpp
+++ LeetCode/ReverseLinkedList.cpp
@@ -1,13 +1,5 @@
 /**
  * Iterative approach
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
  */
 class Solution {
 public:
@@ -34,14 +26,6 @@ public:
 
 /**
  * Recursive approach
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
  */
 class Solution {
 public:
