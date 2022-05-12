c99c61f79d8e82426f6fc914cd2b709deb2de3f6
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2022-05-12 17:18:25 +0900

    feat: solve sum square difference

---
+++ ProjectEuler/p6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int x = 0, y = 0;
+    for(int i = 1; i<=100; ++i){
+        x += pow(i, 2);
+        y += i;
+    }	
+    y = pow(y, 2);
+    
+    cout << y - x;
+	return 0;
+}
