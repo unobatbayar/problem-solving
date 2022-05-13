e6a7fbb5f7e9d1c55705384029a2a2014f2cfda0
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2022-05-13 15:14:43 +0900

    feat: solve 10001st prime

---
+++ ProjectEuler/p7.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool IsPrime(int n)
+{
+    if (n == 2 || n == 3){
+        cout << "2 or 3 found, prime number." << endl;
+        return true;
+    }
+        
+
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0){
+        cout << "0 or 1 or even or divisible by 3 number found " << endl;
+        return false;
+    }
+        
+
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        cout << "Checking " << i << endl;
+        
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+
+
+int main() {
+	int target = 10001;
+	int count = 0;
+	int number = 2;
+	
+	bool found = false;
+	
+	while(!found){
+	    if(IsPrime(number)) count++;
+	    
+	    if(count == target){
+	        cout << "Found: " << number << endl;
+	        found = true;
+	        break;
+	    }
+	    number++;
+	}    
+
+	return 0;
+}
