7387013b5440b8709c9f6c7813f2b575ea316301
Uno <38866249+unobatbayar@users.noreply.github.com>
2022-08-03 14:49:05 +0900

    solve 405A Gravity Flip

---
+++ Codeforces/405A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr_size;
+    cin >> arr_size;
+    
+    int arr[arr_size];
+    for(int i = 0; i<arr_size; ++i){
+        cin >> arr[i];
+    }
+    
+    sort(arr, arr + arr_size);
+    
+    
+    for (int i = 0; i < arr_size; ++i){
+        cout << arr[i] << " ";
+    }
+        
+	// your code goes here
+	return 0;
+}
