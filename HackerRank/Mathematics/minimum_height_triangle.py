e4ee77efc09f897990872da4f91909add53d73f3
Uno <38866249+unobatbayar@users.noreply.github.com>
2023-11-16 18:01:04 +0900

    feat: solve Minimum Height Triangle
    https://www.hackerrank.com/challenges/lowest-triangle/problem

---
+++ HackerRank/Mathematics/minimum_height_triangle.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lowestTriangle' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER trianglebase
+#  2. INTEGER area
+#
+
+def lowestTriangle(base, area):
+    # Write your code here
+    return math.ceil((2*area)/base)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    trianglebase = int(first_multiple_input[0])
+
+    area = int(first_multiple_input[1])
+
+    height = lowestTriangle(trianglebase, area)
+
+    fptr.write(str(height) + '\n')
+
+    fptr.close()
