99fc475d7f2139c18cce6b27eee1df6e36a7d1ec
Uno <38866249+unobatbayar@users.noreply.github.com>
2024-03-27 11:05:55 +0900

    solve: 704_binary_search.py

---
+++ LeetCode/704_binary_search.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # define pointers (not traditional c++ pointers)
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = (left + right)//2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                right = middle - 1
+            else:
+                left = middle + 1
+
+        # not found
+        return -1
+
+        
