111f7b3ca81f0b27238cda7b72efccd4dbce1642
Uno <38866249+unobatbayar@users.noreply.github.com>
2024-04-11 16:11:28 +0900

    Update 242_valid_anagram.py

--- LeetCode/Roadmap/Arrays&Hashing/242_valid_anagram.py
+++ LeetCode/Roadmap/Arrays&Hashing/242_valid_anagram.py
@@ -3,15 +3,12 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
+            
         for c in s:
-            if c in t:
-                new_s = s.replace(c, "")
-                new_t = t.replace(c, "")
-                if len(new_s) != len(new_t):
-                    return False
-            else:
+            new_s = s.replace(c, "")
+            new_t = t.replace(c, "")
+            if len(new_s) != len(new_t):
                 return False
-
+                
         return True
         
