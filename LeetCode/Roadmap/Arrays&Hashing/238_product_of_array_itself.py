d0fe8c259673217742440f620c477572c5806fd5
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2024-06-14 19:34:18 +0900

    solve: Product of Array Except Self

--- LeetCode/Roadmap/Arrays&Hashing/238_product_of_array_itself.py
+++ LeetCode/Roadmap/Arrays&Hashing/238_product_of_array_itself.py
@@ -1,15 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ans = []
+        first = []
+        second = []
+
         length = len(nums)
-
+        temp = nums[::-1]
         # first loop
-        ans.append(nums[0])
-        for i in range(1, length):
-            ans.append(nums[i-1] * nums[i])
-
-        # second loop
         for i in range(0, length):
-            ans[i] = i
-        
+            if i == 0:
+                first.append(nums[i])
+                second.append(temp[i])
+                continue
+            first.append(first[i-1] * nums[i])
+            second.append(second[i-1] * temp[i])
+
+        second.reverse()
+
+        ans = []
+        # get final answer
+        for i in range(0, length):
+            # edge cases
+            if i == 0:
+                ans.append(second[i+1])
+            elif i == length - 1:
+                ans.append(first[i - 1])
+            else:
+                ans.append(first[i-1] * second[i+1])
         return ans
