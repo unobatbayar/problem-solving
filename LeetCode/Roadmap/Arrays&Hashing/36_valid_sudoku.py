6a79eef049a20c15f7085eea29934a378f92124c
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2024-06-16 23:42:42 +0900

    chore: add minor progress on Valid Sudoku

--- LeetCode/Roadmap/Arrays&Hashing/36_valid_sudoku.py
+++ LeetCode/Roadmap/Arrays&Hashing/36_valid_sudoku.py
@@ -1,6 +1,13 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for row in board:
-            for element in row:
-                print(element)
-        return False
+        for i in range (0, len(board)):
+            numbers = set()
+            for j in range(0, len(board[i])):
+                num = board[i][j]
+                if num == '.':
+                    continue
+                if num in numbers:
+                    return False
+                else:
+                    numbers.add(num)
+        return True
