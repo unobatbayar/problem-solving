c94b14d1f99dd3aa01ba047a271d13176b079783
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2024-06-17 12:30:07 +0900

    chore: complete row and column check

--- LeetCode/Roadmap/Arrays&Hashing/36_valid_sudoku.py
+++ LeetCode/Roadmap/Arrays&Hashing/36_valid_sudoku.py
@@ -1,13 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range (0, len(board)):
-            numbers = set()
-            for j in range(0, len(board[i])):
-                num = board[i][j]
-                if num == '.':
-                    continue
-                if num in numbers:
-                    return False
+        for y in range (0, len(board)):
+            row_nums, col_nums = set(), set()
+
+            for x in range(0, len(board)):
+                row = board[y][x]
+                col = board[x][y]
+                # check row
+                if row != '.':
+                    if row in row_nums:
+                        return False
                 else:
-                    numbers.add(num)
+                    row_nums.add(row)
+                # check col
+                if col != '.':
+                    if col in col_nums:
+                        return False
+                else:
+                    col_nums.add(col)
         return True
