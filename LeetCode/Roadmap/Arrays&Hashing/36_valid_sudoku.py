f751388ed5d1e0b0fa47783a1686edf81a33b034
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2024-06-18 00:20:04 +0900

    chore: retry solution

--- LeetCode/Roadmap/Arrays&Hashing/36_valid_sudoku.py
+++ LeetCode/Roadmap/Arrays&Hashing/36_valid_sudoku.py
@@ -1,21 +1,7 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for y in range (0, len(board)):
-            row_nums, col_nums = set(), set()
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        boxes = defaultdict(set)
 
-            for x in range(0, len(board)):
-                row = board[y][x]
-                col = board[x][y]
-                # check row
-                if row != '.':
-                    if row in row_nums:
-                        return False
-                else:
-                    row_nums.add(row)
-                # check col
-                if col != '.':
-                    if col in col_nums:
-                        return False
-                else:
-                    col_nums.add(col)
         return True
