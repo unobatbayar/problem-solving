5f48503183c6f1f420c212d1383d5ce48b398707
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2024-06-18 20:10:47 +0900

    feat: solve Valid Sudoku

--- LeetCode/Roadmap/Arrays&Hashing/36_valid_sudoku.py
+++ LeetCode/Roadmap/Arrays&Hashing/36_valid_sudoku.py
@@ -1,7 +1,22 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = defaultdict(set)
-        rows = defaultdict(set)
+        rows = defaultdict(set) # x
+        cols = defaultdict(set) # y 
         boxes = defaultdict(set)
+
+        for r in range(0, len(board)):
+            for c in range(0, len(board)):
+                num = board[r][c]
+                box = (r//3)*3 + (c//3)
+
+                if num == '.':
+                    continue
+
+                if num in rows[r] or num in cols[c] or num in boxes[box]:
+                    return False
+                
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[box].add(num)
 
         return True
