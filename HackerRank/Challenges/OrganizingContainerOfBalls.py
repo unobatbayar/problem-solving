c364e1a80a56f81e8a399235978a98dd9bbe8291
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2024-06-21 20:25:15 +0900

    chore: add minor progress, start the problem

--- HackerRank/Challenges/OrganizingContainerOfBalls.py
+++ HackerRank/Challenges/OrganizingContainerOfBalls.py
@@ -1,2 +1,48 @@
 # https://www.hackerrank.com/challenges/organizing-containers-of-balls
 # This is a problem I've been given during job interview in 2019. Which I've failed to solve.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'organizingContainers' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts 2D_INTEGER_ARRAY container as parameter.
+#
+
+def organizingContainers(container):
+    # Write your code here
+    length = len(container) # container count is same as ball type
+    
+    
+    for i in range(0, length):
+        for k in range(0, length):
+            
+    
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        container = []
+
+        for _ in range(n):
+            container.append(list(map(int, input().rstrip().split())))
+
+        result = organizingContainers(container)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
