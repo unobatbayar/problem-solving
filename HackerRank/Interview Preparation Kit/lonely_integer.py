3fda67b0c4c1207cab0fa43bf282b75998b72b35
Usukhbayar Batbayar <38866249+unobatbayar@users.noreply.github.com>
2024-08-29 17:57:20 +0800

    Create lonely_integer.py

---
+++ HackerRank/Interview Preparation Kit/lonely_integer.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    integers = set()
+    unique_integers = []
+    
+    for item in a:
+        if item in integers:
+            unique_integers.remove(item)
+        else:
+            unique_integers.append(item)
+            integers.add(item)
+
+    return unique_integers[0]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
