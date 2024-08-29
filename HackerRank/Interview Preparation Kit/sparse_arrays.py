2bd8ffc07e2eba3dea46727aa8edd44af7838088
Usukhbayar Batbayar <38866249+unobatbayar@users.noreply.github.com>
2024-08-29 14:34:23 +0800

    Create sparse_arrays.py

---
+++ HackerRank/Interview Preparation Kit/sparse_arrays.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import defaultdict
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    # Write your code here
+    counts = defaultdict(int)
+    for s in strings:
+        counts[s] += 1
+    
+    result = []
+    for q in queries:
+        result.append(counts[q])
+        
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
