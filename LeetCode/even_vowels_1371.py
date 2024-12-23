4560e4f4a8ddd042c6206ba88ac71e496f5c15fc
Usukhbayar Batbayar <38866249+unobatbayar@users.noreply.github.com>
2024-12-23 11:27:28 +0900

    Update even_vowels_1371.py

--- LeetCode/even_vowels_1371.py
+++ LeetCode/even_vowels_1371.py
@@ -28,3 +28,41 @@ class Solution:
                 # If it's the first time we see this bitmask, store the index
                 seen[bitmask] = i
         return max_length
+
+## Used for debugging 
+s = "lkjkjeel"
+bitmask = 0
+max_length = 0
+seen = {0: -1} # bitmask 0 at index -1
+
+for i in range(len(s)):
+    c = s[i]
+    if c == 'a':
+        bitmask ^= 1 << 0  # Flip the 0th bit for 'a'
+    elif c == 'e':
+        bitmask ^= 1 << 1  # Flip the 1st bit for 'e'
+    elif c == 'i':
+        bitmask ^= 1 << 2  # Flip the 2nd bit for 'i'
+    elif c == 'o':
+        bitmask ^= 1 << 3  # Flip the 3rd bit for 'o'
+    elif c == 'u':
+        bitmask ^= 1 << 4  # Flip the 4th bit for 'u'
+    
+    print(s[:i+1])
+    print(bin(bitmask))
+    
+    # Check if the current bitmask has been seen before
+    if bitmask in seen:
+        # Calculate the length of the valid substring
+        max_length = max(max_length, i - seen[bitmask])
+        #print(s[i:seen[bitmask]])
+        print("is seen")
+        print(max_length)
+    else:
+        # If it's the first time we see this bitmask, store the index
+        seen[bitmask] = i
+        print("adding")
+
+    print("")
+
+print(max_length)
